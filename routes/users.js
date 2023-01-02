15eade1d2c628dacf552a65298ecfddec03e11cb
ignacioborraz <ignacioborraz@hotmail.com>
2023-01-02 20:34:45 -0300

    settings

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-import express from'express';
-let router = express.Router();
+import express from 'express'
+let router = express.Router()
+import schema from '../schemas/signup.schema.js'
+import validator from '../middlewares/validator.js'
+import accountExistsSignUp from '../middlewares/accountExistsSignUp.js'
+import accountExistsSignIn from '../middlewares/accountExistsSignIn.js'
+import accountHasBeenVerified from './../middlewares/accountHasBeenVerified.js'
+import mustSignIn from '../middlewares/mustSignIn.js'
+import controller from '../controllers/users.controller.js'
+const { signup,signin,signintoken,signout,read } = controller
+import passport from '../config/passport.js'
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-export default router;
+router.post('/signup',accountExistsSignUp,validator(schema),signup)
+router.post('/signin',accountExistsSignIn,accountHasBeenVerified,signin)
+router.post('/token',passport.authenticate('jwt', { session:false }),mustSignIn,signintoken)
+router.put('/signout',passport.authenticate('jwt', { session:false }),signout)
+router.get('/',read)
+
+export default router
