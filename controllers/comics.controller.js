d428eba638f6371841969406496969425b793c93
Gustasco <gustavo.espinoza.aliste@gmail.com>
2023-01-04 15:25:34 -0300

    añadi response

--- controllers/comics.controller.js
+++ controllers/comics.controller.js
@@ -1,14 +1,15 @@
 import {Comic} from '../models/comic.models.js'
+import defaultResponse from '../config/response.js'
 
 let comicControl ={
     create: async(req, res)=> {
         try {
             const {author_id, company_id, title, photo, description, category } = req.body
             await Comic.create({author_id, company_id, title, photo, description, category})
-            req.status(201).json({
-                success: true,
-                response: "Logrado",
-            })
+            req.body.success = true
+            req.body.sc = 201
+            req.body.data = 'Comic created'
+            return defaultResponse(req,res)
         }catch(error){
             console.log(error);
         }
