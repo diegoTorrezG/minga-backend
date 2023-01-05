0713f032f058ecbc8053838028a2098714d56a10
LucianaRovere <luciana.rovere96@gmail.com>
2023-01-05 10:19:32 -0300

    el ultimo commit de chapter

--- models/data/chapters.js
+++ models/data/chapters.js
@@ -4,15 +4,15 @@ import {Chapter} from '../Chapter.js'
 
  let chapters = [
     {
-        comic_id: "holita",
-        title: "capitulo 1",
-        pages: [1],
+        comic_id: "63b5c1db823a8ae629f3802e",
+        title: "jujutsu kaisen",
+        pages: ["https://static.wikia.nocookie.net/jujutsu-kaisen/images/f/f6/JJK_New_Key_Visual.jpg/revision/latest?cb=20200914020414&path-prefix=es","https://static.wikia.nocookie.net/jujutsu-kaisen/images/0/0e/Volume_1.png/revision/latest?cb=20200905220554&path-prefix=es"],
         order: 3
         
     },{
-        comic_id: "holita2",
+        comic_id: "63b5c1db823a8ae629f3802f",
         title: "Dragon Ball",
-        pages: [2],
+        pages: ["https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/35e4ac6339f5fdcc164160a5755790cd.jpe","https://static.wikia.nocookie.net/doblaje/images/4/4a/DragonBall.jpeg/revision/latest?cb=20200915224214&path-prefix=es"],
         order: 5
     }
  ]
